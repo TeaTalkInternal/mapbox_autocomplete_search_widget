3a7267af037e22d1d7828fcf9122a604f963ed88
KevinVishalSaldanha <vishalkevin11@gmail.com>
2021-03-26 08:53:38 +0530

    Added Package and example project version 0.0.1

---
+++ example/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:example/example_page_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+//Please generate your Access-Token from here https://docs.mapbox.com/help/glossary/access-token/
+void main() {
+  runApp(
+    ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+      ),
+      home: ExamplePageWidget(),
+    );
+  }
+}
